42beafc224f33a801d5d2877f5eed729e0f15dcf
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-23 21:09:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
-require relative "./user.rb"
+require_relative "./user.rb"
 
 class Student < User
 
   # initializes an instance with an empty array of KNOWLEDGE
   def initialize
-    KNOWLEDGE = []
+    @knowledge = []
   end # initialize
 
   # accepts a string as an element and "teaches" it to the Student
   # by adding it to the KNOWLDEG array
   def learn(knowledge_str)
-    KNOWLEDGE << knowledge_str
+    @knowledge << knowledge_str
   end # learn
 
 end # Student class
